146247833a5224cd19391e35f386497b9319f84e
Mike Bronner <mike@genealabs.com>
2020-08-02 20:26:56 +0000

    Removed laraberg map file references .

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -19,13 +19,10 @@ class FieldServiceProvider extends ServiceProvider
             Nova::script('nova-gutenberg', __DIR__ . '/../dist/js/field.js');
             Nova::script("nova-gutenberg-react", "https://unpkg.com/react@16.8.6/umd/react.production.min.js");
             Nova::script("nova-gutenberg-react-dom", "https://unpkg.com/react-dom@16.8.6/umd/react-dom.production.min.js");
-            // Nova::script("nova-gutenberg-jquery", "https://code.jquery.com/jquery-1.12.4.min.js");
-            Nova::script('nova-gutenberg-laraberg', base_path("vendor/van-ons/laraberg/public/js/laraberg.js"));
-            // Nova::script('nova-gutenberg-laraberg-map', base_path("vendor/van-ons/laraberg/public/js/laraberg.js.map"));
+            Nova::script('nova-gutenberg-laraberg', __DIR__ . "/../dist/js/laraberg.js");
 
+            Nova::style('nova-gutenberg-laraberg', __DIR__ . '/../dist/css/laraberg.css');
             Nova::style('nova-gutenberg', __DIR__ . '/../dist/css/field.css');
-            Nova::style('nova-gutenberg-laraberg', base_path("vendor/van-ons/laraberg/public/css/laraberg.css"));
-            // Nova::style('nova-gutenberg-laraberg-map', base_path("vendor/van-ons/laraberg/public/css/laraberg.css.map"));
         });
     }
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,8 @@
 const mix = require("laravel-mix");
 
 mix
-    .setPublicPath('dist')
-    .js('./resources/js/field.js', 'js')
-    .postCss('./resources/css/field.css', 'css')
-;
+  .setPublicPath("dist")
+  .js("./resources/js/field.js", "js")
+  .copy("./vendor/van-ons/laraberg/public/js/laraberg.js", "dist/js")
+  .postCss("./resources/css/field.css", "css")
+  .copy("./vendor/van-ons/laraberg/public/css/laraberg.css", "dist/css");
