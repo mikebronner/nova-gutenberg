e76f242010f82bce047dd5b1bd41f8938a8440a5
Mike Bronner <mike@genealabs.com>
2020-09-12 23:44:16 +0000

    WIP

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -19,9 +19,9 @@ class FieldServiceProvider extends ServiceProvider
             Nova::script('nova-gutenberg', __DIR__ . '/../dist/js/field.js');
             Nova::script("nova-gutenberg-react", "https://unpkg.com/react@16.8.6/umd/react.production.min.js");
             Nova::script("nova-gutenberg-react-dom", "https://unpkg.com/react-dom@16.8.6/umd/react-dom.production.min.js");
-            Nova::script('nova-gutenberg-laraberg', __DIR__ . "/../vendor/van-ons/laraberg/public/js/laraberg.js");
+            Nova::script('nova-gutenberg', __DIR__ . '/../dist/js/laraberg.js');
 
-            Nova::style('nova-gutenberg-laraberg', __DIR__ . '/../vendor/van-ons/laraberg/public/css/laraberg.css');
+            Nova::style('nova-gutenberg', __DIR__ . '/../dist/css/laraberg.css');
             Nova::style('nova-gutenberg', __DIR__ . '/../dist/css/field.css');
         });
     }
