3ed5272947f06464076569f47c38727e9a7b884e
Mike Bronner <mike@genealabs.com>
2020-09-13 00:31:58 +0000

    Rebuild assets.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,11 @@
 const mix = require("laravel-mix");
 
 mix
-  .setPublicPath("dist")
-  .js("./resources/js/field.js", "js")
-  .postCss("./resources/css/field.css", "css")
+    .setPublicPath("dist")
+    .js("./resources/js/field.js", "js")
+    .postCss("./resources/css/field.css", "css")
+    .copy("vendor/van-ons/laraberg/public/css/laraberg.css", "dist/css")
+    .copy("vendor/van-ons/laraberg/public/css/laraberg.css.map", "dist/css")
+    .copy("vendor/van-ons/laraberg/public/js/laraberg.js", "dist/js")
+    .copy("vendor/van-ons/laraberg/public/js/laraberg.js.map", "dist/js")
 ;
