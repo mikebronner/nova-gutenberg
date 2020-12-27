b5f44e1d5466c17efd0fc0f2db80a17b8bdbceeb
Bhavesh Deshmukh <deshmukhn8@gmail.com>
2020-12-27 10:39:20 +0530

    Fixed issues while reinitilizing editor

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -2,24 +2,53 @@
 import { FormField, HandlesValidationErrors } from 'laravel-nova'
 
 export default {
-    mixins: [
-        FormField,
-        HandlesValidationErrors,
-    ],
+  mixins: [
+    FormField,
+    HandlesValidationErrors,
+  ],
 
-    props: [
-        'field',
-        'resourceId',
-        'resourceName',
-    ],
+  props: [
+    'field',
+    'resourceId',
+    'resourceName',
+  ],
 
-    data: function () {
-        return {
-            blankLaraberg: {},
-        };
+  data: function () {
+    return {
+      blankLaraberg: {},
+    };
+  },
+
+  beforeUpdate: function () {
+    this.removeEditor();
+  },
+
+  mounted: function () {
+    this.blankLaraberg = Object.assign({}, window.Laraberg);
+    this.initializeEditor();
+  },
+
+  updated: function () {
+    window.Laraberg = Object.assign({}, this.blankLaraberg);
+    this.initializeEditor();
+  },
+
+  methods: {
+    initializeEditor: function () {
+      let editor = document.getElementById("laraberg__editor");
+      this.removeEditor();
+
+      if (editor !== null) {
+        editor.remove();
+      }
+
+      Laraberg.init(this.field.name, {
+        laravelFilemanager: true,
+      });
     },
-
-    beforeUpdate: function () {
+    removeEditor: function () {
+      // Don't wanna break everything just because something goes wrong here
+      try {
         const blocks = window.wp.data.select('core/blocks').getBlockTypes().map(bt => bt.name);
         const {removeBlockTypes} = window.wp.data.dispatch('core/blocks');
         const {__experimentalTearDownEditor} = window.wp.data.dispatch('core/editor');
@@ -28,63 +57,40 @@ export default {
         __experimentalTearDownEditor();
 
         if (((window.Laraberg || {}).editor || false) !== false) {
-            window.wp.element.unmountComponentAtNode(window.Laraberg.editor);
+          window.wp.element.unmountComponentAtNode(window.Laraberg.editor);
         }
+      } catch (e) {}
+    },
+    setInitialValue: function () {
+      this.value = this.field.value || '';
     },
 
-    mounted: function () {
-        this.blankLaraberg = Object.assign({}, window.Laraberg);
-        this.initializeEditor();
+    fill: function (formData) {
+      formData.append(this.field.attribute, Laraberg.getContent());
     },
 
-    updated: function () {
-        window.Laraberg = Object.assign({}, this.blankLaraberg);
-        this.initializeEditor();
+    handleChange: function (value) {
+      this.value = value;
     },
-
-    methods: {
-        initializeEditor: function () {
-            let editor = document.getElementById("laraberg__editor");
-
-            if (editor !== null) {
-                editor.remove();
-            }
-
-            Laraberg.init(this.field.name, {
-                laravelFilemanager: true,
-            });
-        },
-
-        setInitialValue: function () {
-            this.value = this.field.value || '';
-        },
-
-        fill: function (formData) {
-            formData.append(this.field.attribute, Laraberg.getContent());
-        },
-
-        handleChange: function (value) {
-            this.value = value;
-        },
-    },
+  },
 }
 </script>
 
 <template>
-    <default-field
-        :field="field"
-        :errors="errors"
-        :fullWidthContent="true"
+  <default-field
+      :field="field"
+      :errors="errors"
+      :fullWidthContent="true"
+  >
+    <template
+        slot="field"
     >
-        <template
-            slot="field"
-        >
             <textarea
                 :id="field.name"
                 :name="field.name"
                 :placeholder="field.name"
                 :value="value"
             ></textarea>
-        </template>
-    </default-field>
+    </template>
+  </default-field>
 </template>
