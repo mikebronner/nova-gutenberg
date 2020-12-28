1ed61ace62302fcdb396aa13057840acf370e346
Mike Bronner <mike@genealabs.com>
2020-12-28 13:52:54 +0000

    Fixed reloading of data in Nova, and disabling of welcome message.

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,114 +1,135 @@
 <script>
-import {FormField, HandlesValidationErrors} from 'laravel-nova'
+    import {FormField, HandlesValidationErrors} from 'laravel-nova';
 
-export default {
-    mixins: [
-        FormField,
-        HandlesValidationErrors,
-    ],
+    export default {
+        mixins: [
+            FormField,
+            HandlesValidationErrors,
+        ],
 
-    props: [
-        'field',
-        'resourceId',
-        'resourceName',
-    ],
+        props: [
+            'field',
+            'resourceId',
+            'resourceName',
+        ],
 
-    data: function () {
-        return {
-            blankLaraberg: {},
-        };
-    },
-
-    beforeUpdate: function () {
-        this.removeEditor();
-    },
-
-    mounted: function () {
-        this.blankLaraberg = Object.assign({}, window.Laraberg);
-        this.initializeEditor();
-    },
-
-    beforeDestroy: () => {
-        // This is required as this.removeEditor might not be available in some cases
-        try {
-            const blocks = window.wp.data.select('core/blocks').getBlockTypes().map(bt => bt.name);
-            const {removeBlockTypes} = window.wp.data.dispatch('core/blocks');
-            const {__experimentalTearDownEditor, resetBlocks} = window.wp.data.dispatch('core/editor');
-
-            removeBlockTypes(blocks);
-            __experimentalTearDownEditor();
-            resetBlocks(parse(this.value));
-        } catch (e) {}
-
-        if (((window.Laraberg || {}).editor || false) !== false) {
-            window.wp.element.unmountComponentAtNode(window.Laraberg.editor);
-        }
-
-        window.Laraberg.editor = undefined
-    },
-
-    updated: function () {
-        window.Laraberg = Object.assign({}, this.blankLaraberg);
-        this.initializeEditor();
-        if (this.value != "") {
-            Laraberg.setContent(this.value)
-        }
-    },
-
-    methods: {
-        initializeEditor: function () {
-            let editor = document.getElementById("laraberg__editor");
-
-            if (editor !== null) {
-                this.removeEditor();
-                editor.remove();
-            }
-            Laraberg.init(this.field.name, {
-                laravelFilemanager: true,
-            });
-
-            const {toggleFeature} = window.wp.data.dispatch('core/edit-post');
-
-            const {isFeatureActive} = wp.data.select('core/edit-post');
-
-            this.field.disable_welcome && isFeatureActive("welcomeGuide") && toggleFeature('welcomeGuide');
+        data: function () {
+            return {
+                blankLaraberg: {},
+            };
         },
 
-        removeEditor: function () {
+        beforeUpdate: function () {
+            this.removeEditor();
+        },
+
+        mounted: function () {
+            this.blankLaraberg = Object.assign({}, window.Laraberg);
+            this.initializeEditor();
+        },
+
+        beforeDestroy: function () {
             try {
-                const blocks = window.wp.data.select('core/blocks').getBlockTypes().map(bt => bt.name);
-                const {removeBlockTypes} = window.wp.data.dispatch('core/blocks');
-                const {__experimentalTearDownEditor, resetBlocks} = window.wp.data.dispatch('core/editor');
+                const blocks = window.wp
+                    .data
+                    .select('core/blocks')
+                    .getBlockTypes()
+                    .map(function (bt) {
+                        return bt.name;
+                    });
+                const {removeBlockTypes} = window.wp
+                    .data
+                    .dispatch('core/blocks');
+                const {__experimentalTearDownEditor, resetBlocks} = window.wp
+                    .data
+                    .dispatch('core/editor');
 
                 removeBlockTypes(blocks);
                 __experimentalTearDownEditor();
                 resetBlocks(parse(this.value));
-
-            } catch (e) {}
+            } catch (exception) {}
 
             if (((window.Laraberg || {}).editor || false) !== false) {
                 window.wp.element.unmountComponentAtNode(window.Laraberg.editor);
             }
 
-            window.Laraberg.editor = undefined
+            window.Laraberg.editor = undefined;
         },
 
-        setInitialValue: function () {
-            this.value = this.field.value || '';
+        updated: function () {
+            window.Laraberg = Object.assign({}, this.blankLaraberg);
+            this.initializeEditor();
+
+            if (this.value !== "") {
+                Laraberg.setContent(this.value);
+            }
         },
 
-        fill: function (formData) {
-            formData.append(this.field.attribute, Laraberg.getContent());
-            // Remove the session item as we no longer need it,
-            // The post will always be 1 as Laraberg is mocking WP API
-            sessionStorage.removeItem("wp-autosave-block-editor-post-1")
-        },
+        methods: {
+            initializeEditor: function () {
+                let editor = document.getElementById("laraberg__editor");
 
-        handleChange: function (value) {
-            this.value = value;
+                if (editor !== null) {
+                    this.removeEditor();
+                    editor.remove();
+                }
+
+                Laraberg.init(this.field.name, {
+                    laravelFilemanager: true,
+                });
+
+                const {toggleFeature} = window.wp.data.dispatch('core/edit-post');
+                const {isFeatureActive} = wp.data.select('core/edit-post');
+
+                this.field.disable_welcome
+                    && isFeatureActive("welcomeGuide")
+                    && toggleFeature('welcomeGuide');
+            },
+
+            removeEditor: function () {
+                try {
+                    const blocks = window.wp
+                        .data
+                        .select('core/blocks')
+                        .getBlockTypes()
+                        .map(function (bt) {
+                            return bt.name;
+                        });
+                    const {removeBlockTypes} = window.wp
+                        .data
+                        .dispatch('core/blocks');
+                    const {__experimentalTearDownEditor, resetBlocks} = window.wp
+                        .data
+                        .dispatch('core/editor');
+
+                    removeBlockTypes(blocks);
+                    __experimentalTearDownEditor();
+                    resetBlocks(parse(this.value));
+                } catch (e) {}
+
+                if (((window.Laraberg || {}).editor || false) !== false) {
+                    window.wp
+                        .element
+                        .unmountComponentAtNode(window.Laraberg.editor);
+                }
+
+                window.Laraberg.editor = undefined;
+            },
+
+            setInitialValue: function () {
+                this.value = this.field.value || "";
+            },
+
+            fill: function (formData) {
+                formData.append(this.field.attribute, Laraberg.getContent());
+                sessionStorage.removeItem("wp-autosave-block-editor-post-1");
+            },
+
+            handleChange: function (value) {
+                this.value = value;
+            },
         },
-    },
-}
+    };
 </script>
 
 <template>
--- src/Gutenberg.php
+++ src/Gutenberg.php
@@ -4,15 +4,19 @@ use Laravel\Nova\Fields\Field;
 
 class Gutenberg extends Field
 {
-    /**
-     * The field's component.
-     *
-     * @var string
-     */
     public $component = 'nova-gutenberg';
 
-    public function disableWelcome($value = true) {
-        $this->withMeta(['disable_welcome' => $value]);
+    public function disableWelcome(): self
+    {
+        $this->withMeta(['disable_welcome' => true]);
+
+        return $this;
+    }
+
+    public function enableWelcome(): self
+    {
+        $this->withMeta(['disable_welcome' => false]);
+
         return $this;
     }
 }
